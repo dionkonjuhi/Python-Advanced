ee20f75b796065def1a026eb382395aaf088f1e8
dionkonjuhi <dionkonjuhiii70@gmail.com>
2026-02-10 17:49:04 +0100

    comit

---
+++ lists.py
@@ -0,0 +1,19 @@
+
+
+to_do_lists = ["pay the course", "come to the course", "do the project", "failed"]
+
+first_item = to_do_lists[0]
+second_item = to_do_lists[1]
+third_item = to_do_lists[2]
+fourth_item = to_do_lists[3]
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shopping_list = ['apple', 'watermelon' ,' melon']
+shopping_list.append('orange')
+print(shopping_list)
+
+
---
+++ variables.py
@@ -0,0 +1,16 @@
+temperature = 37.3
+name = ('Dion')
+
+print(type(temperature))
+print(type(name))
+
+age = 21
+print("age is: ", age)
+
+age=age+1
+print("age now is: ",age)
+
+first_name = "Dion"
+last_name = "Konjuhi"
+full_name = first_name +" "+  last_name
+print(full_name)
