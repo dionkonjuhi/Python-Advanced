c332ba7aa47d89497e158e26a6567a16b7623822
dionkonjuhi <dionkonjuhiii70@gmail.com>
2026-06-18 17:41:08 +0200

    commit

--- lesson28/app/crud.py
+++ lesson28/app/crud.py
@@ -1,8 +1,5 @@
 from typing import List, Optional
 import sqlite3
-
-from lesson2.dictionary import items
-from lesson24.database import cursor
 from models import Item
 from database import get_db_connection
 
@@ -10,7 +7,7 @@ def create_item(item: Item) -> Item:
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO items (name, description) VALUES (?,?)",
+        "INSERT INTO items (name, description) VALUES (?, ?)",
         (item.name, item.description)
     )
     conn.commit()
@@ -20,38 +17,37 @@ def create_item(item: Item) -> Item:
 
 def get_items() -> List[Item]:
     conn = get_db_connection()
-    items = con.execute("SELECT * FROM items").fetchall()
+    items = conn.execute("SELECT * FROM items").fetchall()
     conn.close()
-    return [Item(**dict(item))] for item in items
+    return [Item(**dict(item)) for item in items]
 
 def get_item(item_id: int) -> Optional[Item]:
     conn = get_db_connection()
-    item = conn.execute("SELECT * FROM items WHERE id = ?" (item_id,)).fetchall()
+    item = conn.execute("SELECT * FROM items WHERE id = ?", (item_id,)).fetchone()
     conn.close()
     if item is None:
         return None
     return Item(**dict(item))
 
-def update_item(item_id: int, item: Item) ->Optional[Item]
+def update_item(item_id: int, item: Item) -> Optional[Item]:
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute(
         "UPDATE items SET name = ?, description = ? WHERE id = ?",
-        (items.name, item.description, item_id)
+        (item.name, item.description, item_id)
     )
-
     conn.commit()
     updated = cursor.rowcount
     conn.close()
     if updated == 0:
-        return  None
+        return None
     item.id = item_id
     return item
 
-def delete_item(item_id: id) -> bool:
+def delete_item(item_id: int) -> bool:
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM items id = ?", (item_id))
+    cursor.execute("DELETE FROM items WHERE id = ?", (item_id,))
     conn.commit()
     deleted = cursor.rowcount
     conn.close()
